80aad493233b5fe7317864fa3f2b221b2c453be3
vitviki <vtyagi1991@gmail.com>
2024-07-17 09:23:51 +0530

    setup initial backend server

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+const databaseURL = process.env.MONGO_DB_CONNECTION_URL;
+
+// Setup cors headers, cookie parser and json parser.
+app.use(
+  cors({
+    origin: [process.env.CORS_ORIGIN],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+
+// Connect to DB and start the server
+mongoose
+  .connect(databaseURL)
+  .then(() => {
+    const server = app.listen(PORT, () => {
+      console.log(`Server running at port: https://localhost:${PORT}`);
+    });
+    console.log("DB connection: successful");
+  })
+  .catch((err) => console.log(err.message));
