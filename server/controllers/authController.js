3ba989c11245178c863f2ba56eb1b8a93d8a29f5
vitviki <vtyagi1991@gmail.com>
2024-07-17 13:31:33 +0530

    sign-up and login controllers

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,8 +1,110 @@
-export const signUp = async (req, res) => {};
+import jwt from "jsonwebtoken";
+import User from "../models/UserModel.js";
+import { compare } from "bcrypt";
 
-export const login = async (req, res) => {};
+const maxAge = 3 * 24 * 60 * 60 * 1000;
+const createToken = (email, userId) => {
+  return jwt.sign({ email, userId }, process.env.JWT_KEY, {
+    expiresIn: maxAge,
+  });
+};
 
-export const getProfile = async (req, res) => {};
+export const signUp = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if the email already exists. If it does, return an error.
+    const userFound = await User.findOne({ email });
+
+    if (userFound) {
+      return res
+        .status(400)
+        .send("Email already in use. Please try with another email");
+    }
+
+    const user = await User.create({ email, password });
+
+    return res.status(201).json({
+      message: "Account created successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(400)
+      .json({ message: "Internal server error", data: error.mesage });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Search for user using email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        message:
+          "No user with the given email found. Please use a different email or sign up",
+      });
+    }
+
+    // Check if the password is correct.
+    const auth = await compare(password, user.password);
+
+    if (!auth) {
+      return res
+        .status(404)
+        .json({ message: "Incorrect password. Please try again" });
+    }
+
+    // create a cookie.
+    res.cookie("jwt", createToken(email, user._id), {
+      maxAge,
+      secure: true,
+      sameSite: "None",
+    });
+
+    return res.status(200).json({
+      message: "Login successful",
+      user: {
+        id: user._id,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        profilePicturePath: user.profilePicturePath,
+        profileSetup: user.profileSetup,
+        backgroundColorCode: user.backgroundColorCode,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(400)
+      .json({ message: "Internal server error", data: error.mesage });
+  }
+};
+
+export const getProfile = async (req, res) => {
+  // try {
+  //   // Search for user profile using the userId sent in request.
+  //   console.log(req.userId);
+  //   const user = await User.findById(req.userId);
+  //   if (!user) {
+  //     return res.status(404).json({ message: "User not found" });
+  //   }
+  //   return res.status(200).json({
+  //     message: "User found",
+  //     user,
+  //   });
+  // } catch (error) {
+  //   console.log(error);
+  //   res
+  //     .status(400)
+  //     .json({ message: "Internal server error", data: error.mesage });
+  // }
+};
 
 export const updateProfile = async (req, res) => {};
 
